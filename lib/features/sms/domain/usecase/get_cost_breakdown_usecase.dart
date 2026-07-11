aae85d365d076a67f192640741ab00e07828fdcd
Earbaj <earbajsaria3@gmail.com>
2026-07-11 12:55:14 +0600

    Rename imports for cost breakdown and repository

--- lib/features/sms/domain/usecase/get_cost_breakdown_usecase.dart
+++ lib/features/sms/domain/usecase/get_cost_breakdown_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failures.dart';
-import '../entities/cost_breakdown.dart';
-import '../repositories/sms_repository.dart';
+import '../entity/cost_breakdown_entity.dart';
+import '../repository/sms_repository.dart';
 
 class GetCostBreakdownUseCase {
   final SmsRepository repository;
