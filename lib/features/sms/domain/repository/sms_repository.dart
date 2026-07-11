c1b2b84f596932366d43faa7f615af92d1fbc63e
Earbaj <earbajsaria3@gmail.com>
2026-07-11 13:13:05 +0600

    Fix import paths in sms_repository.dart
    Updated import paths for cost_breakdown_entity and sms_response_entity.

--- lib/features/sms/domain/repository/sms_repository.dart
+++ lib/features/sms/domain/repository/sms_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failures.dart';
-import '../entities/cost_breakdown_entity.dart';
-import '../entities/sms_response_entity.dart';
+import '../entity/cost_breakdown_entity.dart';
+import '../entity/sms_response_entity.dart';
 
 abstract class SmsRepository {
   Future<Either<Failure, SmsResponse>> sendSms(String to, String body);
